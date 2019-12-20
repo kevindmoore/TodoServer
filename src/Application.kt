505a6b9c104c3d5bb058744389c6ffeab5aa0154
Kevin Moore <kevindavmoore@gmail.com>
2019-12-19 21:07:50 -0800

    Heroku attempt 2

--- src/Application.kt
+++ src/Application.kt
@@ -22,7 +22,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(Thymeleaf) {
         setTemplateResolver(ClassLoaderTemplateResolver().apply {
             prefix = "templates/thymeleaf/"
